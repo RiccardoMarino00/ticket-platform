c391e20e1728ac8a6294f2000966b1439420c560
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-11-22 16:32:28 +0100

    first commit

---
+++ database/migrations/2024_11_22_151805_add_operator_id_foreign_key_to_tickets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->unsignedBigInteger('operator_id');
+            $table->foreign('operator_id')->references('id')->on('operators')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropForeign(['operator_id']);
+            $table->dropColumn('operator_id');
+        });
+    }
+};
